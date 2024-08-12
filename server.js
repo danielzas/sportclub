777c6362f5f5928acd3c04032fba14c658d5df03
danielzas <daniel.esteban.aguero@gmail.com>
2024-08-12 20:06:43 -0300

    cambio en la url del script.js por la de render

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'public'), { index: 'index.html' }));
+
 
 app.post('/buscar-socio', async (req, res) => {
   const { documento } = req.body;
