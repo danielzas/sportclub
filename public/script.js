69bc5a5691aff4a1f556cf8606d80012033ac362
danielzas <daniel.esteban.aguero@gmail.com>
2024-08-12 15:22:31 -0300

    Subida inicial del proyecto

---
+++ public/script.js
@@ -0,0 +1,37 @@
+document.getElementById('buscar').addEventListener('click', async () => {
+    const documento = document.getElementById('documento').value;
+  
+    if (!documento) {
+      alert('Por favor, ingresa un documento.');
+      return;
+    }
+  
+    try {
+      const response = await axios.post('http://localhost:3000/buscar-socio', {
+        documento: documento,
+      });
+  
+      const data = response.data;
+      console.log(data);
+      displayResult(data);
+    } catch (error) {
+      console.error('Error al buscar el socio:', error);
+      alert('Hubo un error al buscar el socio. Por favor, intenta de nuevo.');
+    }
+  });
+  
+  function displayResult(data) {
+    const resultDiv = document.getElementById('result');
+    if(data.Response)
+    {
+      console.log('Socio Válido');
+      estado='<p style="color:green;font-size:30px;"><strong>Socio Válido</strong></p>';
+    }else{
+      console.log('Socio Inexistente');
+      estado='<p style="color:red;font-size:30px;"><strong>Socio Inexistente</strong></p>';
+    }
+    resultDiv.innerHTML = `
+      <h3>Información del Socio</h3>${estado};
+    `;
+  }
+  
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public'), { index: 'index.html' }));
+
+app.post('/buscar-socio', async (req, res) => {
+  const { documento } = req.body;
+
+  try {
+    const response = await axios({
+      method: 'post',
+      url: 'https://integraciones.prod.sportclub.com.ar/xl/socio/',
+      headers: {
+        'api-key': 'zOaLXtdje2xUQZUSeSItxF7KlHcEXYKPX0k4EDFpOM0yWy70B1LQCq4Ktyn7AZrj'
+      },
+      data: {
+        documento: documento,
+      },
+    });
+    console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error al buscar el socio:', error);
+    res.status(500).json({ error: 'Error al buscar el socio' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
