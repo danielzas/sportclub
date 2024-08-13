48b313ca0c79d3588ef5031b31f1dfe5dee68f0a
danielzas <daniel.esteban.aguero@gmail.com>
2024-08-13 11:45:32 -0300

    cambios en la directivas de hemlet.

--- server.js
+++ server.js
@@ -11,13 +11,17 @@ app.use(express.json());
 app.use(cors());
 
 // Configurar Helmet para agregar políticas de seguridad
+// Configurar Helmet para agregar políticas de seguridad
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      defaultSrc: ["'none'"],
-      fontSrc: ["'self'", "https://sportclub-pinq.onrender.com"],
-      scriptSrc: ["'self'"],
-      styleSrc: ["'self'"],
+      defaultSrc: ["'self'", "https:"],  // Permitir todo desde el mismo origen y HTTPS
+      fontSrc: ["'self'", "https:", "data:"],  // Permitir fuentes desde el mismo origen, HTTPS y data URIs
+      scriptSrc: ["'self'"],  // Permitir scripts desde el mismo origen
+      styleSrc: ["'self'", "https:"],  // Permitir estilos desde el mismo origen y HTTPS
+      imgSrc: ["'self'", "https:", "data:"],  // Permitir imágenes desde el mismo origen, HTTPS y data URIs
+      connectSrc: ["'self'", "https:"],  // Permitir conexiones desde el mismo origen y HTTPS
+
     },
   })
 );
